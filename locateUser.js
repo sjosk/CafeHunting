18aca0cccb8a667d9ce0360167bce44e2f47f61d
S.J. Esther Wu <146037423+sjosk@users.noreply.github.com>
2023-11-07 22:26:15 +0000

    Create locateUser.js
    Link to the button 'locateButton'

---
+++ locateUser.js
@@ -0,0 +1,58 @@
+// Define Taipei city bounds
+var TAIPEI_BOUNDS = {
+    north: 25.210,
+    south: 24.790,
+    west: 121.456,
+    east: 121.658
+  };
+  
+  // Define Taipei city center
+  var TAIPEI_CENTER = new google.maps.LatLng(25.0330, 121.5654);
+  
+  function locateUser() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+              var userLat = position.coords.latitude;
+              var userLng = position.coords.longitude;
+              var userPos = new google.maps.LatLng(userLat, userLng);
+  
+              if (userLat >= TAIPEI_BOUNDS.south && userLat <= TAIPEI_BOUNDS.north &&
+                  userLng >= TAIPEI_BOUNDS.west && userLng <= TAIPEI_BOUNDS.east) {
+                  // If user is in the area of Taipei, set user's location as center and add a marker
+                  map.setCenter(userPos);
+                  new google.maps.Marker({
+                      position: userPos,
+                      map: map,
+                      title: 'Here you are!'
+                  });
+              } else {
+                  // If user is outside of Taipei, show an info window with a message
+                  // Move the map center to Taipei city center
+                  map.setCenter(TAIPEI_CENTER);
+                  var infoWindow = new google.maps.InfoWindow({
+                      content: ' 🚧 No database in your area. We only provide cafe information in Taipei City.',
+                      position: TAIPEI_CENTER // show the message in the center of Taipei
+                  });
+                  infoWindow.open(map);
+              }
+          }, function(error) {
+              // Error handling
+              handleLocationError(true, TAIPEI_CENTER);
+          });
+      } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, TAIPEI_CENTER);
+      }
+  }
+  
+  function handleLocationError(browserHasGeolocation, pos) {
+      var contentString = browserHasGeolocation ?
+          'Error: The Geolocation service failed.' :
+          'Error: Your browser doesn\'t support geolocation.';
+      var infoWindow = new google.maps.InfoWindow({ map: map, position: pos, content: contentString });
+      infoWindow.open(map);
+  }
+  
+  // Button click listener
+  document.getElementById('locateButton').addEventListener('click', locateUser);
+  
