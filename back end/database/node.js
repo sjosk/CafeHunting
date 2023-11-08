fb87e5d5b3e23fade825e50360c6e82987d1e987
zczqy80 <146266229+zczqy80@users.noreply.github.com>
2023-11-08 14:55:48 +0000

    Add files via upload

---
+++ back end/database/node.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+// 读取 JSON 文件
+fs.readFile('cafedata.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('读取文件时出错:', err);
+        return;
+    }
+
+    try {
+        const jsonData = JSON.parse(data);
+
+        // 生成 MySQL 查询语句
+        let sqlQuery = '';
+        jsonData.forEach(item => {
+            const keys = Object.keys(item);
+            const values = Object.values(item);
+
+            const columns = keys.join(', ');
+            const escapedValues = values.map(val => typeof val === 'string' ? `'${val}'` : val).join(', ');
+
+            sqlQuery += `INSERT INTO Cafe (${columns}) VALUES (${escapedValues});\n`;
+        });
+
+        // 输出 SQL 查询语句
+        //console.log(sqlQuery);
+
+        // 如果需要将查询语句保存到文件
+        fs.writeFile('Cafe.sql', sqlQuery, 'utf8', (err) => {
+            if (err) {
+                console.error('写入文件时出错:', err);
+                return;
+            }
+            console.log('SQL 查询语句已保存到 output.sql 文件');
+        });
+
+    } catch (error) {
+        console.error('解析 JSON 时出错:', error);
+    }
+});
