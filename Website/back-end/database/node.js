314133d37e5b3a372bc98e4631484527cff858ed
zczqy80 <146266229+zczqy80@users.noreply.github.com>
2023-11-14 22:34:04 +0000

    add comments

--- Website/back-end/database/node.js
+++ Website/back-end/database/node.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 // 读取 JSON 文件
+// read json
 fs.readFile('cafedata.json', 'utf8', (err, data) => {
     if (err) {
         console.error('读取文件时出错:', err);
@@ -11,6 +12,7 @@ fs.readFile('cafedata.json', 'utf8', (err, data) => {
         const jsonData = JSON.parse(data);
 
         // 生成 MySQL 查询语句
+        // output mysql
         let sqlQuery = '';
         jsonData.forEach(item => {
             const keys = Object.keys(item);
@@ -23,9 +25,11 @@ fs.readFile('cafedata.json', 'utf8', (err, data) => {
         });
 
         // 输出 SQL 查询语句
+        // check for output
         //console.log(sqlQuery);
 
         // 如果需要将查询语句保存到文件
+        // save mqsql to file
         fs.writeFile('Cafe.sql', sqlQuery, 'utf8', (err) => {
             if (err) {
                 console.error('写入文件时出错:', err);
@@ -35,6 +39,7 @@ fs.readFile('cafedata.json', 'utf8', (err, data) => {
         });
 
     } catch (error) {
+        //output error to user
         console.error('解析 JSON 时出错:', error);
     }
 });
